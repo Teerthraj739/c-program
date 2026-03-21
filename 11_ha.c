59a96286f5af5e506726704468967d9ca8ccf71a
Teerthraj Pandey <teerthraj739@gmail.com>
2026-03-21 14:24:49 +0530

    calculator

---
+++ 11_ha.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int main(){
+    float num1, num2, result;
+    char op;
+    printf("Enter first number");
+    scanf("%f", &num1);
+    
+    printf("Enter an operator (+,-,*,/):");
+    scanf(" %c", &op);
+
+    printf("Enter second number");
+    scanf("%f", &num2);
+
+    switch(op){
+        case'+':
+        result = num1 + num2;
+        printf("result=%f\n", result);
+
+        break;
+
+        case'-':
+        result = num1 - num2;
+        printf("result=%f\n", result);
+
+        break;
+
+        case'*':
+        result = num1 * num2;
+        printf("result=%f\n", result);
+
+        break;
+
+        case'/':
+        if(num2 != 0)
+
+        printf("result = %f\n", num1/num2);
+
+        else
+        printf("division is not possible\n");
+
+
+        break;
+        default:
+        printf("Invalid operator\n");
+
+
+    }
+    return 0;
+}
